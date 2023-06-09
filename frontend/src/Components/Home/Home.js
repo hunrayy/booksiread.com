a892f305ae55335437de7b267dea201ba4971847
hunrayy <henryokiyi8@gmail.com>
2023-06-09 13:19:28 +0100

    Landing page created

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,9 +1,11 @@
 import Navbar from "../Sections/Navbar";
+import Hero from "../Sections/hero";
 
 function Home(){
 
     return <>
                 <Navbar></Navbar>
+                <Hero />
             </>
 
 }
